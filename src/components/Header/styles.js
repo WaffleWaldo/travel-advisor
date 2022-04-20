8d658f896985f19e8b855e38325b3859d61c352d
WaffleWaldo <oswaldoortiz0519@gmail.com>
2022-04-20 13:51:20 -0400

    now has display of simple components

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -37,7 +37,7 @@ export default makeStyles((theme) => ({
     inputInput: {
         padding: theme.spacing(1, 1, 1, 0), 
         paddingLeft: `calc(1em + ${theme.spacing(4)}px) `, 
-        transition: theme.transitions.create(width), 
+        transition: theme.transitions.create('width'), 
         width: '100%', 
         [ theme.breakpoints.up('md')]: { 
             width: '20ch' 
